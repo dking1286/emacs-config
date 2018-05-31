c667c195af929ce2027e59e116b3de2bed15229b
Daniel King <daniel.oliver.king@gmail.com>
2018-05-30 21:16:39 -0700

    setup for clojurescript development with figwheel

---
+++ customizations/figwheel.el
@@ -0,0 +1,7 @@
+;; Setting up Cider to use the Figwheel repl by default for ClojureScript
+
+(require 'cider)
+(setq cider-cljs-lein-repl
+      "(do (require 'figwheel-sidecar.repl-api)
+           (figwheel-sidecar.repl-api/start-figwheel!)
+           (figwheel-sidecar.repl-api/cljs-repl))")
--- init.el
+++ init.el
@@ -159,6 +159,9 @@
 ;; Flycheck
 (load "setup-flycheck.el")
 
+;; Figwheel
+(load "figwheel.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
